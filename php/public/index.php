cc0ddacc46802bbef69ba4a3df60a227e6bc6913
gennei <sai.gennei@gmail.com>
2022-07-20 22:27:29 +0900

    mysql の sample data を取得できるところまで

--- php/public/index.php
+++ php/public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'hello world';
+$dsn = sprintf( 'mysql:dbname=%s;host=%s', 'sample', 'mysql');
+$pdo = new PDO($dsn, 'root', 'root');
+
+$stmt = $pdo->prepare('select * from employee');
+$result = $stmt->execute();
+
+if ($result) {
+  var_dump($stmt->fetch());
+}
